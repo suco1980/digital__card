eb25da2f8823d85c54784afd4a5b2924950ead1f
suco1980 <68135493+suco1980@users.noreply.github.com>
2026-02-07 22:17:59 -0500

    sw change

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,31 @@
+// self.addEventListener("install", event => {
+//   console.log("Service Worker instalado");
+// });
+
+// self.addEventListener("activate", event => {
+//   console.log("Service Worker activado");
+// });
+const CACHE_NAME = "khauling-v1";
+const FILES_TO_CACHE = [
+  "/",
+  "/index.html",
+  "/manifest.json",
+  "/icons/icon-192.png",
+  "/icons/icon-512.png"
+];
+
 self.addEventListener("install", event => {
-  console.log("Service Worker instalado");
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE))
+  );
 });
 
 self.addEventListener("activate", event => {
   console.log("Service Worker activado");
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => response || fetch(event.request))
+  );
 });
